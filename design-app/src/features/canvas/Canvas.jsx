907945225747ba009d317d2dae970527d15dd945
Santhosh Natarajappa <80569940+snatarajappa@users.noreply.github.com>
2022-11-18 14:13:14 -0700

    Added daraImage

--- design-app/src/features/canvas/Canvas.jsx
+++ design-app/src/features/canvas/Canvas.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./Canvas.module.css";
-import { Menu } from "../menu/Menu";
-import { SideBar } from "../sidebar/SideBar";
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
+import image1 from "../../images/staples.jpeg";
 
 export function Canvas() {
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
+  const [image, setImage] = useState("");
   const [lineWidth, setLineWidth] = useState(5);
   const [lineColor, setLineColor] = useState("black");
   const [lineOpacity, setLineOpacity] = useState(0.1);
@@ -23,14 +23,14 @@ export function Canvas() {
     ctxRef.current = ctx;
   }, [lineColor, lineOpacity, lineWidth]);
   // Function for starting the drawing
-  const startDrawing = (e) => {
+  const startAction = (e) => {
     ctxRef.current.beginPath();
     ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
     setIsDrawing(true);
   };
 
   // Function for ending the drawing
-  const endDrawing = () => {
+  const endAction = () => {
     ctxRef.current.closePath();
     setIsDrawing(false);
   };
@@ -39,16 +39,22 @@ export function Canvas() {
     if (!isDrawing) {
       return;
     }
-    ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
-
-    ctxRef.current.stroke();
+    // ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    // ctxRef.current.stroke();
+    let rect = canvasRef.current.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    ctxRef.current.drawImage(document.getElementById("img"), x, y, 100, 100);
   };
   return (
     <Grid item xs={10} className={styles.area}>
+      <div className={styles.image}>
+        <img src={image1} alt="" id="img" />
+      </div>
       <canvas
         className={styles.canvas}
-        onMouseDown={startDrawing}
-        onMouseUp={endDrawing}
+        onMouseDown={startAction}
+        onMouseUp={endAction}
         onMouseMove={draw}
         ref={canvasRef}
         width={`900px`}
